5150e6ff86602e84c425e176952f2db7b5182df3
ryanBrittes <87885748+RyanBrittes@users.noreply.github.com>
2023-11-01 19:58:58 +0000

    update

--- Atividade_A2/src/br/com/fichaEscolar/menuAlunos.java
+++ Atividade_A2/src/br/com/fichaEscolar/menuAlunos.java
@@ -8,6 +8,7 @@ public class menuAlunos {
     //Atributos
     private ArrayList<Aluno> alunoList;
     private Scanner scan;
+    Scanner input = new Scanner(System.in);
 
     //Construtor
     public menuAlunos() {
@@ -17,119 +18,158 @@ public class menuAlunos {
     
     //Função responsável por imprimir o menu de opções
     public void menuExecutar(){
+        //Atributos
         int opt;
-        do
-        {
+
+        //Menu de opções utilizado para chamar funções e interagir com o usuário
+        do{
         System.out.println("\n\tDigite uma opção para continuar:\n\n");
         System.out.println("Opcao 1 --> Gerenciar Alunos\n");
         System.out.println("Opcao 2 --> Rendimento Escolar\n");
         System.out.println("Opcao 9 --> Sair\n");
-        opt = this.scan.nextInt();
+        opt = scan.nextInt();
 
+            //Recurso Switch utilizado para chamar funções a depender do que o usuário digitar
             switch(opt){
+                //Função responsável por abrir um submenu com opções para analisar e modificar dados de alunos
                 case 1:
                     this.menuGerenciarAlunos();
                     break;
 
+                //Função responsável por abrir um submenu que analisa os dados da nota de um aluno ou da turma inteira
                 case 2:
                     this.menuRendimentoEscolar();
                     break;
 
+                //Mensagem de finalização do programa, devido ao usuário ter digitado "9"
                 case 9:
                     System.out.println("Finalizando o programa!");
                     break;
 
+                //Mensagem responsável por informar que o usuário digitou algum dado inválido ao que o sistema reconhece
                 default:
                     System.out.println("Opção inválida, tente novamente!");
                     break;
-            } 
-        }while(opt != 9);
+            }
+        }
+        //Condiçao de repetição do loop de "menuExecutar"
+        while(opt != 9);
     }
 
     //Função responsável por abrir o submenu da opção 1 do menuExecutar
     public void menuGerenciarAlunos(){
+        //Atributos
         int opt;
 
+        //Menu de opções utilizado para chamar funções e interagir com o usuário
         do{
             System.out.println("\n\nDigite uma opção:\n\n");
             System.out.println("Opcao 1 --> Adicionar Aluno\n");
             System.out.println("Opcao 2 --> Alterar nome do Aluno\n");
             System.out.println("Opcao 3 --> Excluir Aluno\n");
             System.out.println("Opcao 4 --> Listar Alunos\n");
-            System.out.println("Opcao 5 --> Selecionar uma aluno para:\n");
-            System.out.println("Opcao 0 --> Sair\n");
+            System.out.println("Opcao 5 --> Adicionar nota:\n");
+            System.out.println("Opcao 0 --> Voltar\n");
+            opt = scan.nextInt();
 
-            opt = this.scan.nextInt();
-
+            //Recurso Switch utilizado para chamar funções a depender do que o usuário digitar
             switch (opt) {
+                //Função resposável por adicionar um novo usuário no sistema, coletando o nome e o código do aluno no sistema
                 case 1:
                     this.adicionarAluno();
                     break;
                 
+                //Função responsável por alterar o nome de um usuário no sistema
                 case 2:
                     this.alterarNomeAluno();
                     break;
 
+                //Função responsável por excluir um aluno no sistema
                 case 3:
                     this.excluirAluno();
                     break;
 
+                //Função responsável por lista os alunos no sistema com os atributos de nome e código
                 case 4:
                     this.listarAluno();
                     break;
 
+                //Função responsável por adicionar as notas de um aluno no sistema
                 case 5:
                     this.adicionarNota();
                         break;
 
+                //Mensagem de aviso de transição ao menu anterior
                 case 0:
                     System.out.println("Voltando...");
                     break;
                 
+                //Mensagem responsável por informar que o usuário digitou algum dado inválido no sistema 
                 default:
                 System.out.println("Opção inválida, tente novamente!");
                     break;
             }
-        } while (opt != 0);
+        } 
+        //Condiçao de repetição do loop de "menuGerenciarAluno"
+        while (opt != 0);
     }
     
     //Função responsável por adicionar um aluno
-    private void adicionarAluno(){
+    public void adicionarAluno(){
+        //Atributo do tipo ArrayList Aluno com a função de armazenar a informação do índice ao percorrer a lista
         Aluno alunoAlterar = null;
 
-        System.out.println("Informe o nome do aluno: \n");
-        String nome = scan.next();
+        //Interação com o usuário para solicitar o nome e armazenar em uma variável o valor
+        System.out.println("\nInforme o nome do aluno: ");
+        String nome = input.nextLine();
 
-        System.out.println("Informe o codigo do aluno: \n");
+        //Interação com o usuário para solicitar o código e armazenar em uma variável o valor
+        System.out.println("\nInforme o codigo do aluno: ");
         int codigo = scan.nextInt();
 
+        //Verificação na lista se o código digitado já está vinculado a algum aluno, responsável para evitar duplicidade de código
         for(Aluno al : alunoList){
             if(al.getCodigo() == codigo){
                 alunoAlterar = al;
                 break;
             }
         }
+        //Caso exista algum código ja adicionado na lista será informada uma mensagem solicitando uma nova tentativa com código diferente
         if(alunoAlterar != null){
             System.out.println("O código digitado já está vinculado a um aluno, tente novamente.");
-        } else {
+        } 
+        //Caso não exista nenhum aluno com o código na lista o mesmo será adicionado junto com suas notas
+        else {
+            //Atribuição de valores às notas do aluno
             double a1 = 0;
             double p1 = 0;
             double a2 = 0;
             double p2 = 0;
+
+            //Criação de um novo aluno na lista com notas, nome e código
             alunoList.add(new Aluno(nome, codigo, a1, p1, a2, p2));
         }
     }
 
     //Função responsável por alterar o nome de uma aluno
     private void alterarNomeAluno(){
+        //Verificação se a lista está vazia, caso esteja será solicitado ao usuário para adicionar um aluno para continuar com a ação
         if(alunoList.isEmpty()){
             System.out.println("Lista vazia, adicione um aluno para continuar.");
-        } else {
+        } 
+        //Caso a lista não esteja vazia será solicitado o código do aluno para realizar a alteração do nome
+        else {
+            //Solicitação do código do aluno que o usuário dejesa alterar
             System.out.println("Digite o codigo do aluno que deseja atualizar: ");
             int cod = scan.nextInt();
+
+            //Atributo do tipo ArrayList Aluno com a função de armazenar a informação do índice ao percorrer a lista
             Aluno alunoAlterar = null;
+
+            //Atributo responsável por armazenar a quantidade de índices percorridos
             int tam = 0;
 
+            //Verificação na lista se o código digitado existe na lista
             for(Aluno al : alunoList){
                 if(al.getCodigo() == cod){
                     alunoAlterar = al;
@@ -138,30 +178,47 @@ public class menuAlunos {
                 tam++; 
             }
 
+            //Caso o código digitado não exista, será exibida uma mensagem informando ao usuário que não foi encontrado
             if(alunoAlterar == null){
-                System.out.println("Aluno não encontrado, tente novamente!");
-            } else {
+                System.out.println("\nAluno não encontrado, tente novamente!");
+            } 
+            
+            //Caso seja encontrado, será solicitada a informação de alteração
+            else {
                 System.out.println("Digite o nome que deseja colocar: ");
-                String nomeAlterar = scan.next(); 
+                String nomeAlterar = input.nextLine(); 
 
+                //Atributo responsável por receber a informação de alteração
                 Aluno alterar = alunoList.get(tam);
-                alterar.setNome(nomeAlterar);
-                System.out.println("ALUNO ALTERADO!!");
 
+                //Mensagem informando o usuário que foi alterado
+                System.out.println("\nAluno " + alterar.getNome() + " alterado com sucesso!");
+
+                //Operação responsável por alterar o nome com a informação fornecida pelo usuário
+                alterar.setNome(nomeAlterar);
             }
         }
     }
 
     //Função responsável por excluir um aluno
     private void excluirAluno(){
+        //Verificação se a lista está vazia, caso esteja será solicitado ao usuário para adicionar um aluno para continuar com a ação
         if(alunoList.isEmpty()){
             System.out.println("Lista vazia, adicione um aluno para continuar.");
-        } else {
+        } 
+        //Caso a lista não esteja vazia será solicitado o código do aluno para realizar a remoção do aluno
+        else {
+            //Solicitação do código do aluno que o usuário dejesa excluir
             System.out.println("Digite o codigo do aluno que deseja excluir: ");
             int cod = scan.nextInt();
+
+            //Atributo do tipo ArrayList Aluno com a função de armazenar a informação do índice ao percorrer a lista
             Aluno alunoExcluir = null;
+
+            //Atributo responsável por armazenar a quantidade de índices percorridos
             int tam = 0;
 
+            //Verificação na lista se o código digitado existe na lista
             for(Aluno al : alunoList){
                 if(al.getCodigo() == cod){
                     alunoExcluir = al;
@@ -169,22 +226,38 @@ public class menuAlunos {
                 }
                 tam++;
             }
+
+            //Caso o código digitado não exista, será exibida uma mensagem informando ao usuário que não foi encontrado
             if(alunoExcluir == null){
-                System.out.println("Aluno não encontrado, tente novamente!");
-            } else {
+                System.out.println("\nAluno não encontrado, tente novamente!");
+            } 
+
+            //Caso seja encontrado, será solicitada a informação de excluir
+            else {
+                //Mensagem informando o usuário que foi alterado
+                System.out.println("\nAluno " + alunoExcluir.getNome() + " removido com sucesso!");
+
+                //Operação responsável por remover o aluno com a informação fornecida pelo usuário
                 alunoList.remove(tam);
-                System.out.println("ALUNO EXCLUIDO!!");
             }
         }
     }
 
     //Função responsável por imprimir uma lista dos alunos com seus nomes e códigos
     private void listarAluno(){
+        //Verificação se a lista está vazia, caso esteja será solicitado ao usuário para adicionar um aluno para continuar com a ação
         if(alunoList.isEmpty()){
             System.out.println("Lista vazia, adicione um aluno para continuar.");
-        } else {
+        } 
+        
+        //Caso a lista não esteja vazia será solicitado o código do aluno para realizar a exibição do nome e código dos alunos
+        else {
+            //Atributo responsável por armazenar a quantidade de índices percorridos
             int i = 0;
+
             System.out.println("Lista de alunos: ");
+
+            //Função responsável por percorrer a lista e exibir o nome e código de cada índice da lista até para quando chegar ao fim da lista
             for(Aluno al : alunoList){
                 i++;
                 System.out.println("\nAluno " + i + ": " +  al.getNome() + "\nCodigo: " + al.getCodigo());
@@ -197,14 +270,24 @@ public class menuAlunos {
 
     //Função responsável adicionar notas ao aluno
     private void adicionarNota(){
+        //Verificação se a lista está vazia, caso esteja será solicitado ao usuário para adicionar um aluno para continuar com a ação
         if(alunoList.isEmpty()){
             System.out.println("Lista vazia, adicione um aluno para continuar.");
-            } else {
+            } 
+            
+            //Caso a lista não esteja vazia será solicitado o código do aluno para que o usuário possa adicionar notas ao aluno
+            else {
+                //Solicitação do código do aluno que o usuário dejesa adicionar nota
                 System.out.println("Digite o codigo do aluno que deseja adicionar nota: ");
                 int cod = scan.nextInt();
 
+                //Atributo responsável por armazenar a quantidade de índices percorridos
                 int tam = 0;
+
+                //Atributo do tipo ArrayList Aluno com a função de armazenar a informação do índice ao percorrer a lista
                 Aluno alunoNota = null;
+
+                //Verificação na lista se o código digitado existe na lista
                 for(Aluno al : alunoList){
                     if(al.getCodigo() == cod){
                         alunoNota = al;
@@ -213,17 +296,25 @@ public class menuAlunos {
                     tam++;
                 }
 
+                //Caso o código digitado não exista, será exibida uma mensagem informando ao usuário que não foi encontrado
                 if(alunoNota == null){
                     System.out.println("Aluno não encontrado, tente novamente!");
-                } else {
+                } 
+                
+                //Caso seja encontrado, será exibido um menu com opções para o usuário adicionar cada nota do aluno
+                else {
+
+                    //Atributos
                     int opt;
                     double a1 = 0;
                     double p1 = 0;
                     double a2 = 0;
                     double p2 = 0;
                     
+                    //Atributo responsável por receber a informação do índice que a lista percorreu e parou
                     Aluno alterarNota = alunoList.get(tam);
 
+                    //Menu de opções utilizado para chamar realizar operações e interagir com o usuário
                     do{
                         System.out.println("\n\nDigite uma opção:\n\n");
                         System.out.println("Opcao 1 --> Adicionar a nota A1\n");
@@ -231,11 +322,12 @@ public class menuAlunos {
                         System.out.println("Opcao 3 --> Adicionar a nota A2\n");
                         System.out.println("Opcao 4 --> Adicionar a nota P2\n");
                         System.out.println("Opcao 5 --> Calcular a Média do aluno\n");
-                        System.out.println("Opcao 0 --> Sair\n");
+                        System.out.println("Opcao 0 --> Voltar\n");
+                        opt = scan.nextInt();
 
-                        opt = this.scan.nextInt();
-
-                        switch (opt) {      
+                        //Recurso Switch utilizado para chamar funções a depender do que o usuário digitar
+                        switch (opt) {    
+                            //Operação responsável por solicitar a nota A1 ao usuário e atribuir o valor digitado  
                             case 1:
                                 System.out.println("Digite a nota A1: ");
                                 a1 = scan.nextDouble();
@@ -298,7 +390,7 @@ public class menuAlunos {
             System.out.println("\n\nDigite uma opção:\n\n");
             System.out.println("Opcao 1 --> Analisar o rendimento de um aluno\n");
             System.out.println("Opcao 2 --> Analisar o rendimento da turma\n");
-            System.out.println("Opcao 0 --> Sair\n");
+            System.out.println("Opcao 0 --> Voltar\n");
 
             opt = this.scan.nextInt();
 
